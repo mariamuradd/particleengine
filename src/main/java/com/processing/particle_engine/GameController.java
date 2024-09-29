89ce7678c57c036becf6c6274f264d2231862413
mariamuradd <mmmurad@smu.edu>
2024-09-29 17:35:55 -0500

    update

---
+++ src/main/java/com/processing/particle_engine/GameController.java
@@ -0,0 +1,13 @@
+package com.processing.particle_engine;
+
+import processing.core.PApplet;
+
+public abstract class GameController {
+
+    PApplet main;
+    //Constructor
+    GameController(PApplet main_){
+    main = main_;
+    } 
+    public abstract void draw();
+}
